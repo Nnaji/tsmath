d97c9330bb21421209de01f6c01d23b5f66611d7
Kingsley Nnaji <kingsley.nnaji@gmail.com>
2019-10-02 23:02:16 +0200

    addittion function created

--- test/test.tsmath.ts
+++ test/test.tsmath.ts
@@ -15,10 +15,20 @@ describe('TS MATH FUNCTIONS', () => {
 			expect(one).to.equal(1);
 		});
 	});
+
+	// counter function section
 	describe('counter function', () => {
 		it('should return 3', () => {
 			const three: number = tsmath.counter(2);
 			expect(three).to.equal(3);
+		});
+	});
+
+	// add function section
+	describe('add function', () => {
+		it('should return 2', () => {
+			const total: number = tsmath.add(1, 2);
+			expect(total).to.equal(3);
 		});
 	});
 });
