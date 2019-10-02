e5bd7d7185433867eb2ce5805e5c3e24a30b4773
Kingsley Nnaji <kingsley.nnaji@gmail.com>
2019-10-02 20:16:31 +0200

    completed the counter function

--- test/test.tsmath.ts
+++ test/test.tsmath.ts
@@ -8,8 +8,17 @@ import 'mocha';
 import tsmath from '../src/';
 
 describe('TS MATH FUNCTIONS', () => {
-	it('should return 1', () => {
-		const one: number = tsmath.id(1);
-		expect(one).to.equal(1);
+	// id function section
+	describe('id function', () => {
+		it('should return 1', () => {
+			const one: number = tsmath.id(1);
+			expect(one).to.equal(1);
+		});
+	});
+	describe('counter function', () => {
+		it('should return 3', () => {
+			const three: number = tsmath.counter(2);
+			expect(three).to.equal(3);
+		});
 	});
 });
